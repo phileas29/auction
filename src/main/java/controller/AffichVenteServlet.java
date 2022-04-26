4dacbbb9fd1a09475ceb4d25e53fe26bfda0b6d0
Phileas peron <peronphil@mailoo.org>
2022-04-26 20:33:16 +0200

    test heroku

--- src/main/java/controller/AffichVenteServlet.java
+++ src/main/java/controller/AffichVenteServlet.java
@@ -17,32 +17,25 @@ public class AffichVenteServlet extends HttpServlet{
 	private ArticleManager articleManager = new ArticleManager();
 	private UtilisateurManager utilisateurManager = new UtilisateurManager();
 	
-	 public AffichVenteServlet() {}
-	 
-	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			request.getRequestDispatcher("/WEB-INF/affichVente.jsp").forward(request, response);
-		}
+	public AffichVenteServlet() {
+	}
 
-		protected void doPost(HttpServletRequest request, HttpServletResponse response)
-				throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/affichVente.jsp").forward(request, response);
+	}
 
-			Integer numArticle = 0;
-			ArticleVendu articleVendu = new ArticleVendu();
-			numArticle = Integer.parseInt(request.getParameter("noArticle"));
-			
-			
-			try {
-				articleVendu = articleManager.afficherUnArticle(numArticle);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			request.setAttribute("profil", utilisateur);
-			request.getRequestDispatcher("/WEB-INF/affichProfil.jsp").forward(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-			
-		}
+		Integer numArticle = 0;
+		ArticleVendu articleVendu = new ArticleVendu();
+		numArticle = Integer.parseInt(request.getParameter("noArticle"));
+		
+		
+		articleVendu = articleManager.afficherUnArticle(numArticle);
+		
+		request.setAttribute("profil", null);
+		request.getRequestDispatcher("/WEB-INF/affichProfil.jsp").forward(request, response);
 
-		}
+		
+	}
 }
